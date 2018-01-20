5ad4d9d978cd6105fc5dc74b3c2ae0e8c3f90c08
Patrick Borowy <patrick.borowy@gmail.com>
2018-01-20 21:57:45 +0000

    setup.cfg was missing, corrected license and added download_url

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ setup(name='simplermock',
       url='https://git.vibes.systems/azdacha/SimplerMock',
       author='Patrick Borowy',
       author_email='patrick.borowy@gmail.com',
-      license='BSD3',
+      license='MIT',
       packages=[],
-      zip_safe=False)
+      zip_safe=False,
+      download_url = 'https://github.com/Azdacha/SimplerMock/archive/0.1.tar.gz'
+      keywords = ['testing', 'mock'],
+      classifiers = []      
+)
