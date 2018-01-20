1bf0fad69304018f4203bd095c762ebeb7e801ae
Patrick Borowy <patrick.borowy@gmail.com>
2018-01-20 22:05:31 +0000

    corrected setup.py from missing char

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='simplermock',
       license='MIT',
       packages=[],
       zip_safe=False,
-      download_url = 'https://github.com/Azdacha/SimplerMock/archive/0.1.tar.gz'
+      download_url = 'https://github.com/Azdacha/SimplerMock/archive/0.1.tar.gz',
       keywords = ['testing', 'mock'],
       classifiers = []      
 )
